d78ea57103e8058644a16da5b45d07a24e0b09b8
Honlan <493722771@qq.com>
2015-08-15 16:15:55 +0800

    update sql

--- auction.sql
+++ auction.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50538
  File Encoding         : utf-8
 
- Date: 08/14/2015 17:29:08 PM
+ Date: 08/15/2015 16:15:42 PM
 */
 
 SET NAMES utf8;
@@ -29,6 +29,7 @@ CREATE TABLE `item` (
   `category` varchar(255) NOT NULL,
   `start` int(11) NOT NULL,
   `highest` int(11) NOT NULL DEFAULT '0',
+  `owner` varchar(255) NOT NULL,
   `status` int(1) NOT NULL DEFAULT '0',
   `deadline` varchar(255) NOT NULL,
   `donator` varchar(255) NOT NULL,
@@ -41,7 +42,31 @@ CREATE TABLE `item` (
 --  Records of `item`
 -- ----------------------------
 BEGIN;
-INSERT INTO `item` VALUES ('1', '/img/item/1.jpg', '/img/item/1.jpg,/img/item/2.jpg', '一个测试拍卖品', 'children', '10000', '0', '0', '1443542399', '伦大锤', '这是一个很屌炸天的拍卖品', '伦大锤是一个很腻害的人'), ('2', '/img/item/2.jpg', '/img/item/2.jpg', '另一个测试', 'children', '5000', '0', '0', '1443542399', '比比', '这是个好东西', '比比是个好人');
+INSERT INTO `item` VALUES ('1', '/img/item/1.jpg', '/img/item/1.jpg,/img/item/2.jpg', '一个测试拍卖品', 'children', '10000', '15000', '乖比比', '0', '1443542399', '伦大锤', '这是一个很屌炸天的拍卖品', '伦大锤是一个很腻害的人'), ('2', '/img/item/2.jpg', '/img/item/2.jpg', '另一个测试', 'children', '5000', '8000', '我是谁', '0', '1443542399', '比比', '这是个好东西', '比比是个好人');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `price`
+-- ----------------------------
+DROP TABLE IF EXISTS `price`;
+CREATE TABLE `price` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `itemId` int(11) NOT NULL,
+  `itemTitle` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `mobile` varchar(16) NOT NULL,
+  `company` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL DEFAULT '0',
+  `timestamp` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `price`
+-- ----------------------------
+BEGIN;
+INSERT INTO `price` VALUES ('3', '1', '一个测试拍卖品', '张宏伦', '13262669093', '上海交大', '程序员', '13', '1439552545'), ('4', '1', '一个测试拍卖品', '张宏伦', '13262669093', '上海交大', '程序员', '13', '1439553139'), ('5', '1', '一个测试拍卖品', '张宏伦', '13262669093', '上海交大', '程序员', '0', '1439553275'), ('6', '1', '一个测试拍卖品', '张宏伦', '13262669093', '上海交大', '程序员', '0', '1439553400'), ('7', '1', '一个测试拍卖品', '张宏伦呀', '13262669093', '上海交大', '程序员', '19000', '1439553451'), ('8', '1', '一个测试拍卖品', '张宏伦耶', '13262669093', '上海交大', '程序员', '12000', '1439553776'), ('9', '1', '一个测试拍卖品', '张宏伦', '13262669093', '上海交大', '程序员', '13000', '1439553853'), ('10', '1', '一个测试拍卖品', '乖比比', '13262669093', '上海交大', '程序员', '15000', '1439554306'), ('11', '2', '另一个测试', '乖比比', '13262669093', '上海交大', '程序员', '6000', '1439554371'), ('12', '2', '另一个测试', '我是谁', '13262669093', '上海交大', '程序员', '8000', '1439554856');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
